c729d12c8c11f2c32c97d92179e52bef7dfda1a3
Dronife <d.smeledis@gmail.com>
2021-03-11 04:32:56 +0200

    Fix not working implementation

--- app/Events/ItemControlEvent.php
+++ app/Events/ItemControlEvent.php
@@ -10,7 +10,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class ItemControlEvent implements ShouldBroadcast, ShouldQueue
+class ItemControlEvent implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
